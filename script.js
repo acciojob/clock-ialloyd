e54a2643339656186e9d41cb99eb7acfb4f18d97
ialloyd <135848926+ialloyd@users.noreply.github.com>
2023-07-20 20:20:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+const displayTime = () => {
+    const now = new Date();
+    const timeString = now.toLocaleString('en-IN', { hour12: true });
+    document.getElementById('timer').textContent = timeString;
+}
+
+setInterval(displayTime, 1000);
